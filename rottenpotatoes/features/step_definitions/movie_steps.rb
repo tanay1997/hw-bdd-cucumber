e2062e953bb564d824f3c69ccbf03d8ee55db9b5
EC2 Default User <ec2-user@ip-172-31-18-80.us-east-2.compute.internal>
2021-03-03 01:36:22 +0000

    Part 2 and Part 3 Compeleted

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,7 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.create!(:title => movie[:title],:rating => movie[:rating],:release_date => movie[:release_date])
+    Movie.create(movie)
   end
 end
 
@@ -18,8 +18,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  regexVal = /.*#{e1}.*#{e2}/m
-  (page.body).match(regexVal)
+  (page.body).match?(/#{e1}.*#{e2}/m)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -30,10 +29,16 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  if (uncheck)
+   rating_list.split(', ').each {|rating| step %{I uncheck "ratings_#{rating}"}}
+  else
+   rating_list.split(', ').each {|rating| step %{I check "ratings_#{rating}"}}
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  Movie.all do |movie|
+    (page.body).contains? movie
+  end
 end
